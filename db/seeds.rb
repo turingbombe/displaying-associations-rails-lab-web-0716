b9498fc4eaff598fb3df439e8d4126d0b560b6d6
Charlie Cook <turingbombe@gmail.com>
2016-08-22 18:29:25 -0400

    Displayed all associations

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+a = Artist.create(name:"Dave")
+s = Song.create(title:"The Best")
+a.songs << s
